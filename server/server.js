626d170e514efcfcefdd1474bdfce60e7a6924b3
Khoi Dao <khoidao55@gmail.com>
2021-08-31 13:28:36 -0700

    created webpack and react app is running

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const SERVER_PORT = 3000;
 
 const app = express();
-app.use(bodyParser.json();
-app.use(bodyParser.urlencoded({ extended: true })));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.listen(SERVER_PORT, () => {
   console.log('Listening on 3000');
