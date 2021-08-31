40eb3241c88e547925ed3026d135fece88d60f18
Khoi Dao <khoidao55@gmail.com>
2021-08-31 13:33:30 -0700

    added picture for fruits

---
+++ fruitPicture.js
@@ -0,0 +1,17 @@
+{
+  "Apple": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/apple.png",
+  "Apricot": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/apricot.png",
+  "Banana": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/banana.png",
+  "Blueberry": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/blueberry.png",
+  "Cherry": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/cherry.png",
+  "Guava": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/guava.png",
+  "Lemon": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/lemon.png",
+  "Mango": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/mango.png",
+  "Orange": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/orange.png",
+  "Pear": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/pear.png",
+  "Pineapple": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/pineapple.png",
+  "Raspberry": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/raspberry.png",
+  "Strawberry": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/strawberry.png",
+  "Tomato": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/tomato.png",
+  "Watermelon": "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/watermelon.png"
+}
